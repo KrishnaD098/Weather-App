4e57d3e6b124aa74e866aded7f06376294e42c45
Krishna Dhoot <118916792+KrishnaD098@users.noreply.github.com>
2024-03-29 18:47:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Weather from "../src/components/weather"
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
